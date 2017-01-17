4f885fce277cc96db83edc1434965000007b32e9
SangYeob Bono Yu <deminoth@gmail.com>
2017-01-17 11:58:19 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import Dropdown from './src/Dropdown'
+import Option from './src/Option'
+
+Dropdown.Option = Option
+
+module.exports = Dropdown
---
+++ src/Dropdown.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import {
+  View
+} from 'react-native'
+
+class Dropdown extends React.Component {
+  render() {
+    return <View />
+  }
+}
+
+export default Dropdown
